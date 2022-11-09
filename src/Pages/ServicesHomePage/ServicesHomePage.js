acea89a4d1fd162aab08cf124026c0858df9fd6b
HumairaIrin <humairamuntaha24@gmail.com>
2022-11-09 19:57:28 +0600

    react-photo-view added

--- src/Pages/ServicesHomePage/ServicesHomePage.js
+++ src/Pages/ServicesHomePage/ServicesHomePage.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 import './ServicesHomePage.css'
 
 const ServicesHomePage = ({ service }) => {
-    const { serviceName, price, ratings, details, image } = service;
+    const { serviceName, price, ratings, details, image, _id } = service;
     return (
         <div className="card shadow-xl">
-            <figure><img className=' w-full' src={image} alt="" /></figure>
+            <figure>
+                <PhotoProvider>
+                    <PhotoView key={_id} src={image}>
+                        <img className=' w-full' src={image} alt="" />
+                    </PhotoView>
+                </PhotoProvider>
+            </figure>
             <div className="card-body">
                 <h2 className="card-title text-3xl font-bold">{serviceName}</h2>
-                <p className='text-sm '>{details.slice(0, 60) + '...'}<Link to='/service' className='text-[#fff558] font-bold'> See details</Link> </p>
+                <p className='text-sm '>{details.slice(0, 60) + '...'}<Link to={`/service/${_id}`} className='text-[#fff558] font-bold'> See details</Link> </p>
                 <div className="card-actions justify-end">
                     <button className="btn btn-primary">Buy Now</button>
                 </div>
